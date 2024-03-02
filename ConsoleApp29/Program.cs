57c7a791664c71f91040de031c61ac4a36e9b41a
Victor R. Silva <victorr.silva005@gmail.com>
2024-03-02 10:34:57 -0300

    Finished program

---
+++ ConsoleApp29/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace ConsoleApp29
+{
+    internal class Program
+    {
+        Program()
+        {
+            Console.Write("Enter file full path: ");        //C:\Windows\Temp\online_users.txt
+            string path = Console.ReadLine();
+
+            Dictionary<string, int> candidates = new Dictionary<string, int>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string[] strings = line.Split(',');
+                        string name = strings[0];
+                        int votes = int.Parse(strings[1]);
+
+                        if (candidates.ContainsKey(name))
+                        {
+                            candidates[name] += votes;
+                        }
+                        else
+                        {
+                            candidates[name] = votes;
+                        }
+                    }
+
+                    foreach (var  pair in candidates)
+                    {
+                        Console.WriteLine(pair.Key + ", " + pair.Value);
+                    }
+                }
+                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            _ = new Program();
+        }
+    }
+}
